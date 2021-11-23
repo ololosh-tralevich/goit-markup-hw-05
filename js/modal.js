ac49ecfbd8223aa6f57e770d524525f6102e6a8e
ololosh-tralevich <karpeko76@gmail.com>
2021-11-23 10:59:55 +0200

    update

--- js/modal.js
+++ js/modal.js
@@ -7,9 +7,9 @@
   
     refs.openModalBtn.addEventListener('click', toggleModal);
     refs.closeModalBtn.addEventListener('click', toggleModal);
-  
     function toggleModal() {
       refs.modal.classList.toggle('is-hidden');
     }
-  })();
+
+})();
   
